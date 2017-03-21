bd57554b2bf35a2615422f5ee8eb920f30383374
adityakalia <aditya.kalia@gmail.com>
2017-03-21 12:39:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ chmod +x UrTUpdater_Ded.sh
 ./UrTUpdater_Ded.sh
 chown -R urt:urt ../urbanterror43/
 chmod a+x Quake3-UrT-Ded.x86_64
+cd q3ut4
 wget https://raw.githubusercontent.com/adityakalia/ut/master/server.cfg
+cd ..
 wget https://raw.githubusercontent.com/adityakalia/ut/master/start.sh
 chmod +x start.sh
